4efb1cf6822cea7e758f253af61cab7ef79b7a86
tiaguu <tiagogteodoro@gmail.com>
2024-08-03 17:35:42 +0100

    Integrated evil_twin with server

--- evil-twin/evil_twin.py
+++ evil-twin/evil_twin.py
@@ -5,22 +5,29 @@ import subprocess
 from disassemble import Disassembler
 import signal
 import logging
+import requests
+import aiohttp
 
 def main():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers= [stream_handler]
+    )
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("goodware_folder", type=str, help="Path to the goodware folder")
+    parser.add_argument("server_ip", type=str, help="The server IP address")
     parser.add_argument("dylibs_folder", type=str, help="Path to the dylibs folder")
     parser.add_argument("malware_folder", type=str, help="Path where injected files will be placed")
     
     args = parser.parse_args()
-    
-    goodware_folder = args.goodware_folder
+
+    server_ip = args.server_ip    
     dylibs_folder = args.dylibs_folder
     malware_folder = args.malware_folder
-
-    if not os.path.isdir(goodware_folder):
-        print(f"Error: Path '{goodware_folder}' is not a valid directory.")
-        return 
 
     if not os.path.isdir(dylibs_folder):
         print(f"Error: Path '{dylibs_folder}' is not a valid directory.")
@@ -29,34 +36,35 @@ def main():
     if not os.path.exists(malware_folder):
         os.makedirs(malware_folder)
 
-    goodware_files = []
-    for goodware_file in os.listdir(goodware_folder):
-        if len(goodware_files) < 136:
-            root, extension = os.path.splitext(goodware_file)
-            if extension == '.zip':
-                goodware_files.append(goodware_file)
-
     dylib_files = []
     for dylib_file in os.listdir(dylibs_folder):
         root, extension = os.path.splitext(dylib_file)
         if extension == '.dylib':
             dylib_files.append(dylib_file)
 
-    for index in range(len(goodware_files)):
-        assigned_dylib_index = index % len(dylib_files)
+    malware_instances_number = 136
+    for number in range(malware_instances_number):
+        logging.info(f"––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––")
+        logging.info(f"–––––––––––––––––––––––––––––––––––– {number} –––––––––––––––––––––––––––––––––––––––")
+        logging.info(f"––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––")
+
+        logging.info("Downloading goodware")
+        download_url = f'http://{server_ip}:8000/download/{number}'
+        with requests.get(download_url, stream=True) as response:
+            goodware_zip_path = os.path.join(malware_folder, f'{number}.zip')
+            with open(goodware_zip_path, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+
+        logging.info("Downloaded goodware with success")
+
+        logging.info("Obtaining goodware .ipa")
+
+        assigned_dylib_index = number % len(dylib_files)
         dylib = dylib_files[assigned_dylib_index]
         dylib_path = os.path.join(dylibs_folder, dylib)
 
-        goodware_zip = goodware_files[index]
-        goodware_zip_path = os.path.join(goodware_folder, goodware_zip)
-
-        goodware_name, extension = os.path.splitext(goodware_zip)
-        goodware_unzip = os.path.join(malware_folder, goodware_name)
-
-        dylib_name, extension = os.path.splitext(dylib)
-
-        malware_name = f'{goodware_name}_{dylib_name}'
-        malware_unzip = os.path.join(malware_folder, malware_name)
+        goodware_unzip = os.path.join(malware_folder, str(number))
 
         with zipfile.ZipFile(goodware_zip_path, 'r') as zip_ref:
             zip_ref.extractall(goodware_unzip)
@@ -67,17 +75,40 @@ def main():
                     goodware_txt_path = os.path.join(goodware_unzip, goodware_unzipped_file)
                     os.remove(goodware_txt_path)
                 if extension == '.ipa':
+                    goodware_name = root
                     goodware_ipa_path = os.path.join(goodware_unzip, goodware_unzipped_file)
-                    malware_ipa_path = os.path.join(goodware_unzip, f'{malware_name}.ipa')
 
-            subprocess.check_output(['./inject/inject', goodware_ipa_path, '-d', dylib_path, '--ipa'])
+            dylib_name, extension = os.path.splitext(dylib)
+
+            malware_name = f'{goodware_name}_{dylib_name}'
+            malware_unzip = os.path.join(malware_folder, malware_name)
+            new_goodware_ipa_path = os.path.join(malware_unzip, f'{goodware_name}.ipa')
+            malware_ipa_path = os.path.join(malware_unzip, f'{malware_name}.ipa')
+
+            os.remove(goodware_zip_path)
+            os.rename(goodware_unzip, malware_unzip)
+            os.rename(new_goodware_ipa_path, malware_ipa_path)
+        
+            logging.info("Obtained goodware .ipa")
+
+            logging.info("Injecting malware .dylib into goodware .ipa")
+
+            subprocess.check_output(['./inject/inject', malware_ipa_path, '-d', dylib_path, '--ipa'])
+
+            logging.info("Injected with success")
+
+            def timeout_handler(signum, frame):
+                raise "Function execution exceeded time limit"
+
+            # Register the signal function handler
+            signal.signal(signal.SIGALRM, timeout_handler)
 
             # Set the alarm for 600 seconds (10 minutes)
             signal.alarm(600)
 
             logging.info(f"Disassembling {malware_name}")
 
-            malware_txt_path = os.path.join(goodware_unzip, f'{malware_name}.txt')
+            malware_txt_path = os.path.join(malware_unzip, f'{malware_name}.txt')
 
             Disassembler().extract_disassembly(input_file = malware_ipa_path, output_file = malware_txt_path)
 
@@ -92,13 +123,21 @@ def main():
                 zipf.write(malware_ipa_path, arcname=os.path.basename(malware_ipa_path))
                 zipf.write(malware_txt_path, arcname=os.path.basename(malware_txt_path))
 
-            os.remove(malware_ipa_path)
-            os.remove(malware_txt_path)
+            os.remove(malware_unzip)
 
-            
+            logging.info(f"Uploading {malware_name}")
 
+            filename = os.path.basename(malware_zip)
+            with open(malware_zip, mode='rb') as file:
+                data = {'file': (filename, file, 'application/zip')}
 
-# ./inject testiOS/app.ipa -d  @executable_path/testiOS/libinjectiOS.dylib --ipa
+                response = requests.post('http://{server_ip}:8000/upload/malware', files=data)
+
+                if response.status_code == 200:
+                    logging.info(f"{malware_name} uploaded with success")    
+
+            os.remove(malware_zip)        
+            logging.info("Cleaned up")
 
 
 if __name__ == "__main__":

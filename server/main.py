c2c0d1520be44ae2ff77bb9f8e2beec75b5c93f2
tiaguu <tiagogteodoro@gmail.com>
2024-09-19 21:20:18 +0100

    Updated server for opcodes

--- server/main.py
+++ server/main.py
@@ -57,3 +57,25 @@ async def download_malware(number: int):
         return FileResponse(download_path, media_type='application/zip', filename=alphabetical_files[number])
     except Exception as e:
         return JSONResponse(status_code=400, content={"message": f"Could not download the file: {e}"})
+    
+@app.get("/download/opcodes/malware/{number}")
+async def download_opcodes_malware(number: int):
+    try:
+        files_dir = "malware-opcodes"
+        files = os.listdir(files_dir)
+        sorted_files = sorted(files, key=lambda x: os.path.getsize(os.path.join(files_dir, x)))
+        download_path = os.path.join(files_dir, sorted_files[number])
+        return FileResponse(download_path, media_type='text/plain', filename=sorted_files[number])
+    except Exception as e:
+        return JSONResponse(status_code=400, content={"message": f"Could not download the file: {e}"})
+    
+@app.get("/download/opcodes/goodware/{number}")
+async def download_opcodes_goodware(number: int):
+    try:
+        files_dir = "goodware-opcodes"
+        files = os.listdir(files_dir)
+        sorted_files = sorted(files, key=lambda x: os.path.getsize(os.path.join(files_dir, x)))
+        download_path = os.path.join(files_dir, sorted_files[number])
+        return FileResponse(download_path, media_type='text/plain', filename=sorted_files[number])
+    except Exception as e:
+        return JSONResponse(status_code=400, content={"message": f"Could not download the file: {e}"})
